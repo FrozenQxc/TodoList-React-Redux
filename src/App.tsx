519058a931c4ccc1a63f9c3c36e5278abfad7fe9
Timofei Stepanov <125030768+FrozenQxc@users.noreply.github.com>
2024-03-06 15:38:57 +0300

    :fire:  hot-fix:  bg height

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import TodoList from './components/TodoList'
 export default function App() {
 	const [active, setActive] = useState<boolean>(false)
 	return (
-		<div className=' bg-[#111] h-full md:h-dvh flex-col'>
+		<div className=' bg-[#111] min-h-screen'>
 			{active && <ModalAddTodo setActive={setActive} />}
 			<Header setActive={setActive} />
 			<TodoList />
